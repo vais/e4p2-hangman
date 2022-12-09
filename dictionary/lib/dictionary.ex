eb9daefa33d2f88ce32ead53e7357f10ed422f39
Vais Salikhov <vsalikhov@gmail.com>
2022-12-08 22:42:13 -0500

    Implement random_word

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -4,4 +4,8 @@ defmodule Dictionary do
     |> File.read!()
     |> String.split("\n", trim: true)
   end
+
+  def random_word do
+    Enum.random(word_list())
+  end
 end
--- dictionary/test/dictionary_test.exs
+++ dictionary/test/dictionary_test.exs
@@ -6,4 +6,12 @@ defmodule DictionaryTest do
     word_list = Dictionary.word_list()
     assert length(word_list) == 8879
   end
+
+  test "random_word" do
+    word_list = Dictionary.word_list()
+    word = Dictionary.random_word()
+    another_word = Dictionary.random_word()
+    assert word in word_list
+    assert word != another_word
+  end
 end
