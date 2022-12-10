75baba5063123e5e252d377133781964ec40f938
Vais Salikhov <vsalikhov@gmail.com>
2022-12-09 22:41:51 -0500

    Use a module attribute to store word list

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,8 +1,10 @@
 defmodule Dictionary do
+  @word_list "assets/words.txt"
+             |> File.read!()
+             |> String.split("\n", trim: true)
+
   def word_list do
-    "assets/words.txt"
-    |> File.read!()
-    |> String.split("\n", trim: true)
+    @word_list
   end
 
   def random_word do
