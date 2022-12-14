94be60d2b778f2d2f895289d364c0ec5e96cae03
Vais Salikhov <vsalikhov@gmail.com>
2022-12-13 22:47:22 -0500

    Start on game implementation

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,5 +1,7 @@
 defmodule Hangman do
-  @type game :: any
+  alias Hangman.Impl.Game
+
+  @opaque game :: Game.t()
   @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
   @type tally :: %{
           turns_left: integer,
@@ -9,10 +11,8 @@ defmodule Hangman do
         }
 
   @spec new_game :: game
-  def new_game do
-  end
+  defdelegate new_game, to: Game
 
   @spec make_move(game, String.t()) :: {game, tally}
-  def make_move(_game, _guess) do
-  end
+  defdelegate make_move(game, guess), to: Game
 end
