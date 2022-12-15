a2f03355db86f563a364cf02c87440df03a5d3bb
Vais Salikhov <vsalikhov@gmail.com>
2022-12-15 11:19:10 -0500

    Rearrange the types

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,17 +2,10 @@ defmodule Hangman do
   alias Hangman.Impl.Game
 
   @opaque game :: Game.t()
-  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
-  @type tally :: %{
-          turns_left: integer,
-          game_state: state,
-          letters: list(String.t()),
-          used: list(String.t())
-        }
 
   @spec new_game :: game
   defdelegate new_game, to: Game
 
-  @spec make_move(game, String.t()) :: {game, tally}
+  @spec make_move(game, String.t()) :: {game, Game.tally()}
   defdelegate make_move(game, guess), to: Game
 end
--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,8 +1,17 @@
 defmodule Hangman.Impl.Game do
+  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+
+  @type tally :: %{
+          turns_left: integer,
+          game_state: state,
+          letters: [String.t()],
+          used: [String.t()]
+        }
+
   @type t :: %__MODULE__{
           turns_left: integer,
-          game_state: Hangman.state(),
-          letters: list(String.t()),
+          game_state: state,
+          letters: [String.t()],
           used: MapSet.t(String.t())
         }
 
@@ -11,12 +20,15 @@ defmodule Hangman.Impl.Game do
             letters: [],
             used: MapSet.new()
 
+  @spec new_game :: t
+  @spec new_game(String.t()) :: t
   def new_game(word \\ Dictionary.random_word()) do
     %__MODULE__{
       letters: word |> String.codepoints()
     }
   end
 
+  @spec make_move(t, String.t()) :: {t, tally}
   def make_move(game, _guess) do
     game
   end
