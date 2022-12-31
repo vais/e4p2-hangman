3952d90defadcd5191df1766775a32264906633b
Vais Salikhov <vsalikhov@gmail.com>
2022-12-31 14:18:07 -0500

    Add client API functions to make GenServer calls

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,16 +1,16 @@
 defmodule Hangman do
-  alias Hangman.Impl.Game
+  alias Hangman.Runtime.Server
 
-  @opaque game :: Game.t()
+  def new_game do
+    {:ok, pid} = Server.start_link()
+    pid
+  end
 
-  @spec new_game :: game
-  @spec new_game(String.t()) :: game
-  defdelegate new_game, to: Game
-  defdelegate new_game(word), to: Game
+  def make_move(game, guess) do
+    GenServer.call(game, {:make_move, guess})
+  end
 
-  @spec make_move(game, String.t()) :: {game, Game.tally()}
-  defdelegate make_move(game, guess), to: Game
-
-  @spec tally(game) :: Game.tally()
-  defdelegate tally(game), to: Game
+  def tally(game) do
+    GenServer.call(game, {:tally})
+  end
 end
