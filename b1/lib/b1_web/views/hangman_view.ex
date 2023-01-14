c90378d591e169e10f4222a91be66f7ef18ac65d
Vais Salikhov <vsalikhov@gmail.com>
2023-01-13 23:24:05 -0500

    Extract the form into a helper

--- b1/lib/b1_web/views/hangman_view.ex
+++ b1/lib/b1_web/views/hangman_view.ex
@@ -1,6 +1,28 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
 
+  def make_move_or_play_again(conn, status) when status in [:won, :lost] do
+    button("Play Again", to: Routes.hangman_path(conn, :new))
+  end
+
+  def make_move_or_play_again(conn, _status) do
+    form_for(
+      conn,
+      Routes.hangman_path(conn, :update),
+      [method: :put, as: :make_move],
+      fn form ->
+        [
+          text_input(form, :guess,
+            autocomplete: "off",
+            autofocus: true,
+            maxlength: 1
+          ),
+          submit("Make move")
+        ]
+      end
+    )
+  end
+
   def figure_for(7) do
     """
      ______
